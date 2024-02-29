59f53530cb78fe62761222c1c369ebda76bb5225
Капешко Олег А <oleg.kapeshko@belwest.com>
2024-02-29 10:00:09 +0300

    0.1.1 - fix pathes

--- utils/listen.php
+++ utils/listen.php
@@ -33,7 +33,7 @@ error_reporting(E_ALL);
 
 use GlpiPlugin\Telegramtickets\Telegram;
 
-require_once 'vendor/autoload.php';
-include (__DIR__."/../../inc/includes.php");
+require_once '../vendor/autoload.php';
+include (__DIR__."/../../../inc/includes.php");
 
 Telegram::listen();
